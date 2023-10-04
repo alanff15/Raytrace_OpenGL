6fdf6a8f9cb85c55d818a40b9943bcffe858c9fe
Alan <alanff15@gmail.com>
2023-10-03 22:28:15 -0300

    Meu app básico

--- src/App.cpp
+++ src/App.cpp
@@ -9,12 +9,49 @@
 
 namespace App {
 
-void Setup() {}
+Renderer renderer;
+std::unique_ptr<VertexBuffer> vb;
+std::unique_ptr<VertexArray> vao;
+std::unique_ptr<IndexBuffer> ib;
+std::unique_ptr<Shader> shader;
 
-void Render() {}
+void Setup() {
+  // blend
+  GLCall(glEnable(GL_BLEND));
+  GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+  // vertices
+  float data[] = {-1.0f, -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f};
+  vao = std::make_unique<VertexArray>();
+  vb = std::make_unique<VertexBuffer>(data, 4 * 2 * sizeof(float));
+  VertexBufferLayout vbl;
+  vbl.Push<float>(2);
+  vao->AddBuffer(*vb, vbl);
+  // indices
+  uint32_t indices[] = {0, 1, 2, 2, 3, 0};
+  ib = std::make_unique<IndexBuffer>(indices, 6);
+  // shader
+  shader = std::make_unique<Shader>("../../res/app.glsl");
+  shader->Bind();
+}
 
-void RenderInterface() {}
+void Render() {
+  renderer.Draw(*vao, *ib, *shader);
+}
 
-void Shutdown() {}
+void RenderInterface() {
+  ImGui::Begin("Minha janelinha");
+  ImGui::Text("Área de trabalho");
+  if (ImGui::Button("Ativar")) {
+    std::cout << "Botão de teste" << std::endl;
+  }
+  ImGui::End();
+}
+
+void Shutdown() {
+  vb.release();
+  vao.release();
+  ib.release();
+  shader.release();
+}
 
 }  // namespace App
