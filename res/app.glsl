0e00db672238fdf42708118ea6cd20a18084bc51
Alan <alanff15@gmail.com>
2023-11-11 10:01:54 -0300

    fix shaders

--- res/app.glsl
+++ res/app.glsl
@@ -1,4 +1,4 @@
-const std::string GLSL_STR = R"glsl(#shader vertex
+const std::string GLSL_STR = R"glsl(//#shader vertex
 #version 330 core
 
   layout(location = 0) in vec4 position; out vec2 fragCoord;
@@ -8,9 +8,9 @@ const std::string GLSL_STR = R"glsl(#shader vertex
     fragCoord = position.xy;
   }
 
-#-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- - #
+//-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --//
 
-#shader fragment
+//#shader fragment
 #version 330 core
 
   layout(location = 0) out vec4 fragColor;
